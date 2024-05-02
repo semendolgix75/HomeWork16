209010ce540ea8b9fad4ca0fb5a90fda6d9ca6d0
Semen <semen.dolgix@yandex.ru>
2024-05-02 23:36:05 +0500

    Ready Homework add method Check

--- src/main/java/pro/sky/skyprocalculator/CalculateService.java
+++ src/main/java/pro/sky/skyprocalculator/CalculateService.java
@@ -3,6 +3,8 @@ package pro.sky.skyprocalculator;
 public interface CalculateService {
     String welcomeCalculator();
 
+    boolean checkInput(Integer userNum1, Integer userNum2);
+
     String resultPlus(Integer userNum1, Integer userNum2);
 
     String resultMinus(Integer userNum1, Integer userNum2);
--- src/main/java/pro/sky/skyprocalculator/CalculateServiceI.java
+++ src/main/java/pro/sky/skyprocalculator/CalculateServiceI.java
@@ -4,21 +4,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculateServiceI implements CalculateService {
-
+    @Override
     public String welcomeCalculator() {
         return "<h1>Добро пожаловать в калькулятор!</h1>";
     }
-
+    @Override
+    public boolean checkInput(Integer userNum1, Integer userNum2) {
+        return userNum1 == null || userNum2 == null;
+    }
+    @Override
     public String resultPlus(Integer userNum1, Integer userNum2) {
-        if (userNum1 == null || userNum2 == null) {
+        if (checkInput(userNum1,userNum2)) {
             return "Введены неверные данные";
         } else {
             return userNum1 + " + " + userNum2 + " = " + (userNum1 + userNum2);
         }
     }
-
+    @Override
     public String resultMinus(Integer userNum1, Integer userNum2) {
-        if (userNum1 == null || userNum2 == null) {
+        if (checkInput(userNum1,userNum2)) {
             return "Введены неверные данные";
         } else {
             return userNum1 + " - " + userNum2 + " = " + (userNum1 - userNum2);
@@ -27,7 +31,7 @@ public class CalculateServiceI implements CalculateService {
 
     @Override
     public String resultMultiply(Integer userNum1, Integer userNum2) {
-        if (userNum1 == null || userNum2 == null) {
+        if (checkInput(userNum1,userNum2)) {
             return "Введены неверные данные";
         } else {
             return userNum1 + " * " + userNum2 + " = " + (userNum1 * userNum2);
@@ -36,9 +40,10 @@ public class CalculateServiceI implements CalculateService {
 
     @Override
     public String resultDivide(Integer userNum1, Integer userNum2) {
-        if (userNum1 == null || userNum2 == null || userNum2 == 0) {
+        if (checkInput(userNum1,userNum2)) {
             return "Введены неверные данные";
-        } else {
+        } else if(userNum2 == 0){ return "Деление на ноль"; }
+         else {
             return userNum1 + " / " + userNum2 + " = " + ((float) userNum1 / userNum2);
         }
     }
